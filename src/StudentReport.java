b4cb46b3e3d1ccfab02d14fb9a402060d6524724
Jorge <jorge.nina@gmail.com>
2025-08-27 20:45:31 -0500

    Actualizar StudentReport.java con nuevas funcionalidades

---
+++ src/StudentReport.java
@@ -0,0 +1,8 @@
+class StudentReport {
+    public static void main(String[] args) {
+        System.out.println("Student Report");
+        System.out.println("Name: Jorge");
+        System.out.println("Age: 18");
+        System.out.println("Grade: 9.5");
+    }
+}
